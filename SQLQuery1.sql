87355c08838fa18503cf58adfe88d31449b3ee90
varmasunny <60172523+varmasunny@users.noreply.github.com>
2023-01-18 18:38:51 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿
+
+CREATE PROCEDURE InsertPositionDetails
+(
+    @PositionCode varchar(50),
+    @Description varchar(500),
+    @Year int,
+    @BudgetedStrength int,
+    @CurrentStrength int
+)
+AS
+begin
+    INSERT INTO PositionTable (cPositionCode, vDescription, siYear,ibudgetedstrength,icurrentstrength)
+    values (@PositionCode, @Description,@Year, @BudgetedStrength,@CurrentStrength)
+end
